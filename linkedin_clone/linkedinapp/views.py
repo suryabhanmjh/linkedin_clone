e93ceb1dcd4d513d74ea6b2fc76c0be96cb46259
Suryabhan Singh <suryabhan283808@gmail.com>
2025-07-24 11:16:39 +0530

    Add new images for talent and zevo360 to the static assets

--- linkedin_clone/linkedinapp/views.py
+++ linkedin_clone/linkedinapp/views.py
@@ -35,6 +35,73 @@ def home(request):
             'comments': 75,
             'shares': 2,
         },
+        {
+            'user': 'Prince Kumar',
+            'position': 'Junior Front-End Developer',
+            'profile': 'prince.jpg',
+            'content': '''🚨 !!hashtag#Hiring Hiring Hiring !! 🚨 😎🚀 Position - Junior Front-End Developer 
+             Location - (Remote)
+             Experience:- Intern To 5 Year
+             Stipend: $600 - $4200 per month
+             Day Shift - 11: 00am- 5: 00pm
+             Qualification - Graduation/Freshers''',
+            'image': 'higheri.gif',
+            'likes': 300,
+            'comments': 15,
+            'shares': 4,
+        },
+        {
+            'user': 'Prachi Saxena',
+            'position': '| DSA | Frontend Developer',
+            'profile': 'prachi.jpeg',
+            'content': 'Thrilled to share that I have successfully completed the Deloitte Technology Job Simulation and earned a certification! 😎🚀',
+            'image': 'prschipost.jpeg',
+            'likes': 300,
+            'comments': 55,
+            'shares': 4,
+        },
+        {
+            'user': 'Suryabhan Singh',
+            'position': 'Full Stack Developer',
+            'profile': 'profile.jpg',
+            'content': 'Just completed my LinkedIn clone using Django! 😎🚀',
+            'image': 'linkedin-post.png',
+            'likes': 900,
+            'comments': 75,
+            'shares': 2,
+        },
+        {
+            'user': 'kundan kumar',
+            'position': 'Full Stack Developer',
+            'profile': 'kundan.jpeg',
+            'content': '''🚀 Excel Dashboard Project: Sales Analysis for FNP (Ferns N Petals) 🎁📈
+
+            Just wrapped up an end-to-end Sales Analysis project for FNP – one of India's leading online gifting platforms, using Microsoft Excel only! 🧑‍💻✨''',
+            'image': 'kp.jpeg',
+            'likes': 20,
+            'comments': 2,
+            'shares': 1,
+        },
+        {
+            'user': 'Suryabhan Singh',
+            'position': 'Full Stack Developer',
+            'profile': 'profile.jpg',
+            'content': 'Just completed my LinkedIn clone using Django! 😎🚀',
+            'image': 'linkedin-post.png',
+            'likes': 900,
+            'comments': 75,
+            'shares': 2,
+        },
+        {
+            'user': 'Suryabhan Singh',
+            'position': 'Full Stack Developer',
+            'profile': 'profile.jpg',
+            'content': 'Just completed my LinkedIn clone using Django! 😎🚀',
+            'image': 'linkedin-post.png',
+            'likes': 900,
+            'comments': 75,
+            'shares': 2,
+        },
     ]
     return render(request, 'linkedinapp/home.html', {'posts': posts})
 
